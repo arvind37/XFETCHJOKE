fa601e0d50c4332f6ecd0dd0ac467d22a1f3f425
Arvind <arvind.kumawat37@gmail.com>
2025-12-30 01:32:39 +0530

    updated js and html

--- main.js
+++ main.js
@@ -3,24 +3,26 @@
    const setupEle = document.querySelector('.joke-setup');
    const puchlineEle = document.querySelector('.joke-puchline');
    const tryEle = document.querySelector('.try-again');
-
+   const errorEle = document.querySelector('.error-text');
 
    async function fetchAPI() {
       try {
         setupEle.innerHTML = 'Fetching...'
         puchlineEle.classList.add('hide');
+        buttonEle.disabled = true;
         const response = await fetch('https://official-joke-api.appspot.com/random_joke');
         const text = await response.text();
         const res = JSON.parse(text);
         console.log(res);
-
+        buttonEle.disabled = false;
         puchlineEle.classList.remove('hide');
         setupEle.innerHTML = res.setup;
         puchlineEle.innerHTML = res.punchline;
       } catch(error) {
         setupEle.classList.add('red');
         tryEle.classList.remove('hide');
-        setupEle.innerHTML = 'Could not fetch a joke. Try again.'
+        errorEle.classList.remove('hide');
+        buttonEle.disabled = false;
       }
 
     }
